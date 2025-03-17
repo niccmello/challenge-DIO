986dee0bceb774745d3e741784c30d57e98ac730
niccmello <niccmello13@icloud.com>
2025-03-17 10:47:34 -0300

    fim do projeto

--- challenge2.js
+++ challenge2.js
@@ -1,1 +1,36 @@
 // ranked match calculator
+let result = calcular (100, 30)
+let nível = 0
+
+console.log("Quantidade de vitórias = " + result)
+
+function calcular(vitórias, derrotas){
+    let add = vitórias - derrotas
+    return add
+}
+
+if(result < 10){
+    nível = "Ferro"
+    console.log("Seu ranking atual é Ferro")
+} else if (result >= 11 && result <= 20){
+    nível = "Bronze"
+    console.log("Seu ranking atual é Bronze")
+}else if (result >= 21 && result <= 50){
+    nível = "Prata"
+    console.log("Seu ranking atual é Prata")
+}else if (result >= 51 && result <= 80){
+    nível = "Ouro"
+    console.log("Seu ranking atual é Ouro")
+}else if (result >= 81 && result <= 90){
+    nível = "Diamante"
+    console.log("Seu ranking atual é Diamante")
+}else if (result >= 91 && result <= 100){
+    nível = "Lendário"
+    console.log("Seu ranking atual é Lendário")
+}else if (result >= 101){
+    nível = "Imortal"
+    console.log("Seu ranking atual é Imortal")
+}
+
+console.log(" ")
+console.log("O herói tem saldo de " + result + " vitórias e está no nível de " + nível)
---
+++ test.js
@@ -0,0 +1,8 @@
+let result = calcular (30, 10)
+
+
+
+function calcular(vitórias, derrotas){
+    let add = vitórias - derrotas
+    return add
+}
